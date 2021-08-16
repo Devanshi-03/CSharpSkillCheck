6fe5597248253b93dd9870c943145fe015dc3ced
Devanshi <devanshi.jain@ltts.com>
2021-08-16 15:55:49 +0530

    Added exception handling for problem 1.

--- ProblemOne_IncorrectSolution/Program.cs
+++ ProblemOne_IncorrectSolution/Program.cs
@@ -13,6 +13,7 @@ namespace ProblemOne_IncorrectSolution
             //1. The following code find the sum of all the numbers between 0 and 10000
             //2. The sum of the numbers should be for only numbers that are divisble by 3 or 5
             int result = 0;
+            try {
             for (int i = 1; i <= 10000; i++)
             {
                 if (((i % 3) == 0) || ((i % 5) == 0))
@@ -21,6 +22,13 @@ namespace ProblemOne_IncorrectSolution
                 }
             }
             Console.WriteLine("The sum of all number divisble by 3 and 5 between 1 and 10000 is "+ result);
+            Console.ReadKey();}
+             catch (FormatException f)
+            {
+                Console.WriteLine("Please insert an integer ");
+
+            }
+            
             
         }
     }
